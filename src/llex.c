b83c8bfba04c2e0e2f6a8564d41688c516a0252e
MCJack123 <jackmacwindowslinux@gmail.com>
2020-08-23 22:46:22 -0400

    Added string.pack, packsize, unpack; fixed \xNN

--- src/llex.c
+++ src/llex.c
@@ -279,7 +279,7 @@ static void read_long_string (LexState *ls, SemInfo *seminfo, int sep) {
 static int fromhexdigit(int c) {
   if (c >= '0' && c <= '9') return c - '0';
   else if (c >= 'a' && c <= 'f') return c - 'a' + 10;
-  else if (c >= 'A' && c <= 'F') return c - 'F' + 10;
+  else if (c >= 'A' && c <= 'F') return c - 'A' + 10;
   else return -1;
 }
 
